df9b2024c53b4b00ebd052ebec1ee037d40a25d6
molly <mollynzembi@gmail.com>
2024-03-21 15:09:18 +0300

    creates index.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+const random = 8;
+random >=0;
+
+const num3 =8
+const num4= 2
+const mod = num3/num4
+
+const max= 20;
+
+Math.max(11,12, 19, 18, 20);
